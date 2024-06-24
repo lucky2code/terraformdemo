813ca506aae1f677fa21ae84e23107f0b9976ba3
lucky2code <87842803+lucky2code@users.noreply.github.com>
2024-06-24 14:46:18 +1000

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,12 @@
+provider "azurerm" {
+    version ="2.4.0"
+    features {}
+}
+resource "azure_resource_group" "example" {
+    name = "example"
+    location = "australiaeast"
+}
 
+output "id" {
+    value = data.azure_resource_group.example.id
+}
